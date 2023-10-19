9ddefbc679a0bca94a6e29230cdad6e51aae85be
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-19 14:04:14 -0700

    h

---
+++ convert_number.c
@@ -0,0 +1,101 @@
+#include "main.h"
+
+/**
+ * print_hex - print hex
+ * @ap: argument pointer
+ * @params: parameters struct
+ * Return: n of bytes
+ */
+
+int print_hex(va_list ap, params_t *params)
+{
+unsigned long l;
+char *str;
+int c = 0;
+if (params->l_modifier)
+	l = (unsigned long)va_arg(ap, unsigned long);
+else if (params->h_modifier)
+	l = (unsigned short int)va_arg(ap, unsigned int);
+else
+	l = (unsigned int)va_arg(ap, unsigned int);
+str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+if (params->hashtag_flag && l)
+{
+	*--str = 'x';
+	*--str = '0';
+}
+params->unsign = 1;
+return (c += print_number(str, params));
+}
+
+/**
+ * print_HEX - print hex UPPER
+ * @ap: argument pointer
+ * @params: parameters struct
+ * Return: n of bytes
+ */
+
+int print_HEX(va_list ap, params_t *params)
+{
+unsigned long l;
+char *str;
+int c = 0;
+if (params->l_modifier)
+	l = (unsigned long)va_arg(ap, unsigned long);
+else if (params->h_modifier)
+	l = (unsigned short int)va_arg(ap, unsigned int);
+else
+	l = (unsigned int)va_arg(ap, unsigned int);
+str = convert(l, 16, CONVERT_UNSIGNED, params);
+if (params->hashtag_flag && l)
+{
+	*--str = 'X';
+	*--str = '0';
+}
+params->unsign = 1;
+return (c += print_number(str, params));
+}
+/**
+ * print_octal - print octal 8
+ * @ap: argument pointer
+ * @params: parameters struct
+ * Return: n of bytes
+ */
+int print_octal(va_list ap, params_t *params)
+{
+unsigned long l;
+char *str;
+int c = 0;
+if (params->l_modifier)
+	l = (unsigned long)va_arg(ap, unsigned long);
+else if (params->h_modifier)
+	l = (unsigned short int)va_arg(ap, unsigned int);
+else
+	l = (unsigned int)va_arg(ap, unsigned int);
+str = convert(l, 8, CONVERT_UNSIGNED, params);
+if (params->hashtag_flag && l)
+{
+	*--str = '0';
+}
+params->unsign = 1;
+return (c += print_number(str, params));
+}
+int print_binary(va_list ap, params_t *params)
+{
+unsigned long l;
+char *str;
+int c = 0;
+if (params->l_modifier)
+	l = (unsigned long)va_arg(ap, unsigned long);
+else if (params->h_modifier)
+	l = (unsigned short int)va_arg(ap, unsigned int);
+else
+	l = (unsigned int)va_arg(ap, unsigned int);
+str = convert(l, 2, CONVERT_UNSIGNED, params);
+if (params->hashtag_flag && l)
+{
+	*--str = '0';
+}
+params->unsign = 1;
+return (c += print_number(str, params));
+}
--- specifier.c
+++ specifier.c
@@ -6,24 +6,22 @@
  */
 int (*get_specifier(char *s))(va_list ap, params_t *params)
 {
-specifier_t specifiers[] = 
-{
-	{"c", print_char}
-	{"d", print_int}
-	{"i", print_int}
-	{"s", print_string}
-	{"%", print_percent}
-	{"b", print_binary}
-	{"o", print_octal}
-	{"u", print_unsigned}
-	{"x", print_hex}
-	{"X", print_HEX}
-	{"p", print_address}
-	{"S", print_S}
-	{"c", print_rev}
-	{"c", print_rot13}
-	{NULL, NULL}
-};
+specifier_t specifiers[] = {
+	{"c", print_char},
+	{"d", print_int},
+	{"i", print_int},
+	{"s", print_string},
+	{"%", print_percent},
+	{"b", print_binary},
+	{"o", print_octal},
+	{"u", print_unsigned},
+	{"x", print_hex},
+	{"X", print_HEX},
+	{"p", print_address},
+	{"S", print_S},
+	{"c", print_rev},
+	{"c", print_rot13},
+	{NULL, NULL}};
 int i = 0;
 while (specifiers[i].specifier)
 {
@@ -45,7 +43,7 @@ return (NULL);
 
 int get_print_func(char *s, va_list ap, params_t *params)
 {
-int (*f)(va_listm, params_t *) = get_specifier(s);
+int (*f)(va_list, params_t *) = get_specifier(s);
 if (f)
 	return (f(ap, params));
 return (0);
@@ -122,7 +120,7 @@ if (*s == '*')
 else
 {
 	while (_isdigit(*s))
-		d = d * 10 + (*str++ - '0');
+		d = d * 10 + (*s++ - '0');
 }
 params->width = d;
 return (s);
