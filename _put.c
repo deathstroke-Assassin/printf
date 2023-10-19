7f2ee96368b547a135616aba853573fc5aba2c5c
deathstroke-Assassin <moh2004amed@gmail.com>
2023-10-19 07:00:50 -0700

    s

---
+++ _put.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _puts - puts a char to putchar
+ * @str: string
+ * Return: void
+ */
+int _puts(char *str)
+{
+char *a = str;
+while (*str)
+	putchar(*str++);
+return (str - a);
+}
+/**
+ * _putchar - print string
+ * @c: character
+ * Return: 0
+ */
+int _putchar(int c)
+{
+static int i;
+static char buf[OUTPUT_BUF_SIZE];
+if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+{
+	write(1, buf, i);
+	i = 0;
+}
+if (c != BUF_FLUSH)
+	buf[i++] = c;
+return (i);
+}
--- main.h
+++ main.h
@@ -38,8 +38,8 @@ typedef struct parameters
 	unsigned int hashtag_flag      :1;
 	unsigned int zero_flag         :1;
 	unsigned int minus_flag        :1;
-	unsigned int width             :1;
-	unsigned int precision         :1;
+	unsigned int width;
+	unsigned int precision;
 	unsigned int h_modifier        :1;
 	unsigned int l_modifier        :1;
 	unsigned int plus_flag         :1;
